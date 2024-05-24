ba91cf341391a141abf4a5280af835bc58bce44f
Nguyễn Minh Quang <quang.nm.64cntt@.ntu.edu.vn>
2024-05-24 10:24:07 +0700

    update

--- src/index.js
+++ src/index.js
@@ -2,29 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import MyComponent from './bin';
-import global_en from './translation/en/global.json';
-import global_vie from './translation/vie/global.json';
-import i18next from 'i18next';
-import { I18nextProvider } from 'react-i18next';
-i18next.init({
-    interpolation: { escapeValue: false },
-    lng: 'en',
-    resources: {
-        en: {
-            global: global_en,
-        },
-        vie: {
-            global: global_vie,
-        },
-    },
-});
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <BrowserRouter>
-        <I18nextProvider i18n={i18next}>
-            <App />
-        </I18nextProvider>
-        <MyComponent />
+        <App />
     </BrowserRouter>,
 );

192d95dba04c38ff8501294dfdbf97fc62f51923
truongdx <truong.dx.64cntt@ntu.edu.vn>
2024-05-27 09:13:47 +0700

    update

--- src/components/admin/Profile.js
+++ src/components/admin/Profile.js
@@ -1,6 +1,4 @@
-import React, { useState } from 'react';
 import { Box } from '@mui/material';
-import Header from '../../components/Header';
 import Pr from '../../pages/Pr';
 
 const Profile = () => {
@@ -9,8 +7,6 @@ const Profile = () => {
     return (
         <Box m="20px">
             <Box display="flex" justifyContent="space-between" alignItems="center">
-                {/* <Header title="PROFILE" subtitle="Welcome to Profile Page" /> */}
-                {/* <Header title="PROFILE" /> */}
             </Box>
             <div>
                 <Pr />

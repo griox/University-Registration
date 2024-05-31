80f0ea390c7b94838cfedb93df42baeded58819c
Anh Trương <anh.tq.64cntt@ntu.edu.vn>
2024-05-31 16:56:30 +0700

    update

--- src/pages/Student Manage/Student_table.js
+++ src/pages/Student Manage/Student_table.js
@@ -9,7 +9,6 @@ import {
     PlusCircleOutlined,
     MinusCircleOutlined,
     ManOutlined,
-    UploadOutlined,
 } from '@ant-design/icons';
 import { toast } from 'react-toastify';
 import { Button, Space } from 'antd';
@@ -575,31 +574,33 @@ const StudentList = () => {
                 />
                 <Form form={form} component={false}>
                     <Spin spinning={Loading}>
-                        <Table
-                            components={{
-                                body: {
-                                    cell: EditableCell,
-                                },
-                            }}
-                            dataSource={studentData}
-                            columns={mergedColumns}
-                            scroll={{
-                                x: 900,
-                                y: 'calc(100vh - 300px)',
-                            }}
-                            rowClassName="editable-row"
-                            showSorterTooltip={{
-                                target: 'sorter-icon',
-                            }}
-                            pagination={{
-                                onChange: cancel,
-                                showSizeChanger: true,
-                                showQuickJumper: true,
-                                showTotal: (total) => `${t('title.total')} ${total}`,
-                            }}
-                            rowHoverable={false}
-                            ref={tableRef}
-                        />
+                        <div className="table">
+                            <Table
+                                components={{
+                                    body: {
+                                        cell: EditableCell,
+                                    },
+                                }}
+                                dataSource={studentData}
+                                columns={mergedColumns}
+                                scroll={{
+                                    x: 900,
+                                    y: 'calc(100vh - 300px)',
+                                }}
+                                rowClassName="editable-row"
+                                showSorterTooltip={{
+                                    target: 'sorter-icon',
+                                }}
+                                pagination={{
+                                    onChange: cancel,
+                                    showSizeChanger: true,
+                                    showQuickJumper: true,
+                                    showTotal: (total) => `${t('title.total')} ${total}`,
+                                }}
+                                rowHoverable={false}
+                                ref={tableRef}
+                            />
+                        </div>
                     </Spin>
                 </Form>
             </Space>
--- src/pages/University/AddSchool.js
+++ src/pages/University/AddSchool.js
@@ -421,29 +421,32 @@ const AddSchool = () => {
         <div className="Layout">
             <Form form={form} component={false}>
                 <Space direction="vertical">
-                    <FormAdd UniData={UniData} setUniData={setUniData} />
-                    <Spin spinning={loading}>
-                        <Table
-                            columns={mergedColumns}
-                            dataSource={UniData}
-                            onChange={onChange}
-                            pagination={{
-                                defaultPageSize: 10,
-                                pageSizeOptions: ['10', '20', '40', '100'],
-                                showSizeChanger: true,
-                                showQuickJumper: true,
-                                showTotal: (total) => `${t('title.total')} ${total}`,
-                            }}
-                            scroll={{ x: false, y: 'calc(100vh - 350px)' }}
-                            components={{
-                                body: {
-                                    cell: EditableCell,
-                                },
-                            }}
-                            rowHoverable={false}
-                            ref={tableRef}
-                        />
-                    </Spin>
+                    <div className="table">
+                        <FormAdd UniData={UniData} setUniData={setUniData} />
+                        <Spin spinning={loading}>
+                            <Table
+                                columns={mergedColumns}
+                                dataSource={UniData}
+                                onChange={onChange}
+                                pagination={{
+                                    defaultPageSize: 10,
+                                    pageSizeOptions: ['10', '20', '40', '100'],
+                                    showSizeChanger: true,
+                                    showQuickJumper: true,
+                                    showTotal: (total) => `${t('title.total')} ${total}`,
+                                }}
+                                scroll={{ x: false, y: 'calc(100vh - 350px)' }}
+                                components={{
+                                    body: {
+                                        cell: EditableCell,
+                                    },
+                                }}
+                                rowHoverable={false}
+                                className="table-responsive"
+                                ref={tableRef}
+                            />
+                        </Spin>
+                    </div>
                 </Space>
             </Form>
             <Modal open={isModalDetailVisible} onCancel={handleCancel} onOk={handleOk} width={1000} height={500}>

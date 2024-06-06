f69a44179c3a19b8d9a813c792d93c051b48c739
Ngô Quang Huy <nh920211@gmail.com>
2024-06-06 11:23:17 +0700

    updating chatroom

--- src/pages/ChatRoom/Chat.js
+++ src/pages/ChatRoom/Chat.js
@@ -1,15 +1,40 @@
-import React from 'react'
-import {Collapse,Typography} from 'antd'
-const {Panel} = Collapse;
+import React from 'react';
+import { Select } from 'antd';
+import styled from 'styled-components';
+
+const StyledSelect = styled.div`
+  display: ${localStorage.getItem('selectedMenuItem') === 'ChatRoom' ? 'none' : 'block'};
+`;
+
 export const Chat = () => {
   return (
-      <Collapse ghost>
-        <Panel header="Rooms List" >
-          <Typography.Link>Room 1</Typography.Link>
-          <Typography.Link>Room 2</Typography.Link>
-          <Typography.Link>Room 3</Typography.Link>
-        </Panel>
-      </Collapse>
-  )
-}
+    <StyledSelect>
+      <Select
+        defaultValue="lucy"
+        style={{
+          width: 120,
+        }}
+        options={[
+          {
+            value: 'jack',
+            label: 'Room 1',
+          },
+          {
+            value: 'lucy',
+            label: 'Room 2',
+          },
+          {
+            value: 'Yiminghe',
+            label: 'Room 2',
+          },
+          {
+            value: 'disabled',
+            label: 'Room 3',
+          },
+        ]}
+      />
+    </StyledSelect>
+  );
+};
+
 export default Chat;

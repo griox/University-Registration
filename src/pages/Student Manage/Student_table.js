6f5ce20c7d3124d0d6778a88064776d2918b07d0
Ngô Quang Huy <nh920211@gmail.com>
2024-06-14 08:40:49 +0700

    resize column

--- src/pages/Student Manage/Student_table.js
+++ src/pages/Student Manage/Student_table.js
@@ -511,7 +511,7 @@ const StudentList = () => {
             title: t('table.ID'),
             dataIndex: 'id',
 
-            width: '14%',
+            width: '15%',
             fixed: 'left',
             ...getColumnSearchProps('id'),
             render: (_, record) => (
